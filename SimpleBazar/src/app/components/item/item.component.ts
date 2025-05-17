e6677dcaf0eb5d8b411eb3707435bc3097e64f9c
Gabriel Colla <101674782+erGaBs@users.noreply.github.com>
2025-05-17 23:27:13 +0200

    imbastimento grafico

--- SimpleBazar/src/app/components/item/item.component.ts
+++ SimpleBazar/src/app/components/item/item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Item } from 'src/app/models/item';
 
 @Component({
   selector: 'app-item',
@@ -10,10 +11,105 @@ export class ItemComponent {
 
   id: string | null = null;
 
+  data: any;
+
+  options: any;
+
+  items : Item[] = [
+    { id: 1, name: "Piuma d'angelo", timestamp: "2025-05-01T12:00:00Z", price: "4,397" },
+    { id: 1, name: "Piuma d'angelo", timestamp: "2025-05-02T12:00:00Z", price: "4,384" },
+    { id: 1, name: "Piuma d'angelo", timestamp: "2025-05-03T12:00:00Z", price: "4,200" },
+    { id: 1, name: "Piuma d'angelo", timestamp: "2025-05-04T12:00:00Z", price: "4,100" },
+    { id: 1, name: "Piuma d'angelo", timestamp: "2025-05-05T12:00:00Z", price: "3,77" },
+    { id: 1, name: "Piuma d'angelo", timestamp: "2025-05-06T12:00:00Z", price: "3,997" },
+    { id: 1, name: "Piuma d'angelo", timestamp: "2025-05-07T12:00:00Z", price: "3,497" },
+    { id: 1, name: "Piuma d'angelo", timestamp: "2025-05-08T12:00:00Z", price: "3,697" }
+  ]
+
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
-    // Ora puoi utilizzare this.id come necessario
+    this.getItemData(this.id);
+    this.createGraph()
+  }
+
+  getItemData(id: string | null) {
+    //riempo l'array con i dati dell'oggetto recuperati dall'API
+  }
+
+  createGraph(){
+    this.setGraphOptions()
+        const documentStyle = getComputedStyle(document.documentElement);
+
+        this.data = {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          datasets: [
+              {
+                  label: 'Piume d\'angelo',
+                  fill: false,
+                  borderColor: documentStyle.getPropertyValue('--blue-500'),
+                  yAxisID: 'y',
+                  tension: 0.4,
+                  data: [65, 59, 80, 81, 56, 55, 10]
+              }
+          ]
+      };
+
+    
+  }
+
+
+  setGraphOptions(){
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+    const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+    this.options = {
+      stacked: false,
+      maintainAspectRatio: false,
+      aspectRatio: 0.6,
+      plugins: {
+          legend: {
+              labels: {
+                  color: textColor
+              }
+          }
+      },
+      scales: {
+          x: {
+              ticks: {
+                  color: textColorSecondary
+              },
+              grid: {
+                  color: surfaceBorder
+              }
+          },
+          y: {
+              type: 'linear',
+              display: true,
+              position: 'left',
+              ticks: {
+                  color: textColorSecondary
+              },
+              grid: {
+                  color: surfaceBorder
+              }
+          },
+          y1: {
+              type: 'linear',
+              display: true,
+              position: 'right',
+              ticks: {
+                  color: textColorSecondary
+              },
+              grid: {
+                  drawOnChartArea: false,
+                  color: surfaceBorder
+              }
+          }
+        }
+      };
   }
 }
--- SimpleBazarBE/server.js
+++ SimpleBazarBE/server.js
@@ -5,8 +5,11 @@ const axios = require('axios'); // Assicurati di installare axios con 'npm insta
 const app = express();
 const port = 3000;
 
-// Pianifica la chiamata API ogni giorno alle 14:00
-cron.schedule('5 21 * * *', () => {
+
+/////////////////////////////////////// SCHEDULO LE CHIAMATE ALLE LORO API (ANCORA DA COSTRUIRE I PAYLOAD E SETTARE I VARI ENDPOINT)
+
+// Pianifica la chiamata API ogni giorno alle 8:00
+cron.schedule('0 8 * * *', () => {
   console.log('Eseguo la chiamata API alle 14:00');
   axios.get('https://disease.sh/v3/covid-19/historical/all?lastdays=all')
     .then(response => {
@@ -16,6 +19,45 @@ cron.schedule('5 21 * * *', () => {
       console.error('Errore nella chiamata API:', error);
     });
 });
+
+// Pianifica la chiamata API ogni giorno alle 12:00
+cron.schedule('0 12 * * *', () => {
+    console.log('Eseguo la chiamata API alle 14:00');
+    axios.get('https://disease.sh/v3/covid-19/historical/all?lastdays=all')
+      .then(response => {
+        console.log('Risposta API:', response.data);
+      })
+      .catch(error => {
+        console.error('Errore nella chiamata API:', error);
+      });
+  });
+
+  // Pianifica la chiamata API ogni giorno alle 16:00
+cron.schedule('0 16 * * *', () => {
+    console.log('Eseguo la chiamata API alle 14:00');
+    axios.get('https://disease.sh/v3/covid-19/historical/all?lastdays=all')
+      .then(response => {
+        console.log('Risposta API:', response.data);
+      })
+      .catch(error => {
+        console.error('Errore nella chiamata API:', error);
+      });
+  });
+
+  // Pianifica la chiamata API ogni giorno alle 21:00
+cron.schedule('0 21 * * *', () => {
+    console.log('Eseguo la chiamata API alle 14:00');
+    axios.get('https://disease.sh/v3/covid-19/historical/all?lastdays=all')
+      .then(response => {
+        console.log('Risposta API:', response.data);
+      })
+      .catch(error => {
+        console.error('Errore nella chiamata API:', error);
+      });
+  });
+
+
+/////////////////////////////////////// CHIAMATE ALLE NOSTRE API
 
 app.get('/', (req, res) => {
   res.send('Ciao dal backend Node.js!');
